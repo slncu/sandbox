ff8f8586692ac042af5681f59ae012d991b3ef4e
Shunsuke Urita <s_urita@waku-2.com>
2019-03-07 13:07:46 +0900

    Offline

--- sw/js/index.js
+++ sw/js/index.js
@@ -1,1 +1,9 @@
 console.log('hello,world')
+
+const isOnline = window.navigator.onLine
+
+if (isOnline) {
+  document.body.classList.remove('offline')
+} else {
+  document.body.classList.add('offline')
+}
--- sw/sw.js
+++ sw/sw.js
@@ -1,4 +1,4 @@
-const VERSION = '2'
+const VERSION = '5'
 const PATH = location.hostname === 'localhost' ? ':8888' : '/sandbox/sw'
 const ORIGIN = location.protocol + '//' + location.hostname + PATH
 console.log(VERSION)
