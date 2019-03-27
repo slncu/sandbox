31d95bf291ec0ca661545b5851ab414cfd4faa05
Shunsuke Urita <s_urita@waku-2.com>
2019-03-27 18:34:48 +0900

    Fix

---
+++ substantiall_free/build/precache-manifest.c33ce6fa339bac7111f4fcd10e95c7c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "165d3b96a70538065440",
+    "url": "/sandbox/substantiall_free/build/static/js/runtime~main.18f156d1.js"
+  },
+  {
+    "revision": "fc295bc5c701276b2999",
+    "url": "/sandbox/substantiall_free/build/static/js/main.659d14d6.chunk.js"
+  },
+  {
+    "revision": "a526dbe3ccbd722a3e53",
+    "url": "/sandbox/substantiall_free/build/static/js/2.a5a061d4.chunk.js"
+  },
+  {
+    "revision": "fc295bc5c701276b2999",
+    "url": "/sandbox/substantiall_free/build/static/css/main.c55da67c.chunk.css"
+  },
+  {
+    "revision": "0b184903aa2d6d324fe1d408878128e8",
+    "url": "/sandbox/substantiall_free/build/index.html"
+  }
+];
