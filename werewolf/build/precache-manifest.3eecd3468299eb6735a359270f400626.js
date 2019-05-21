945529f9aff2edb4064c07c2eb009b37ddb9033b
Shunsuke Urita <braundrot.s@gmail.com>
2019-05-21 13:42:48 +0900

    fix gitignore

---
+++ werewolf/build/precache-manifest.3eecd3468299eb6735a359270f400626.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b465579bb836bdaaf5839299441f38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "354214fa105bb46cf795",
+    "url": "/static/css/main.91b3bd44.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "354214fa105bb46cf795",
+    "url": "/static/js/main.0b97f17d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
